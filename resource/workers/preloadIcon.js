df77159822c7e54ab7c8ed5e7e6ef8cca5d0b0ec
Luís Miguel <quicksaver@gmail.com>
2016-10-19 20:36:56 +0100

    avoid InvalidStateError

--- resource/workers/preloadIcon.js
+++ resource/workers/preloadIcon.js
@@ -2,12 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// VERSION 1.0.0
+// VERSION 1.0.1
 
 self.onmessage = function (e) {
 	let iconUrl = e.data.iconUrl;
 
 	let xhr = new XMLHttpRequest();
+	xhr.open('GET', iconUrl, true);
 	xhr.responseType = 'blob';
 	xhr.onload = function () {
 		self.postMessage({ iconUrl, loaded: true });
@@ -15,6 +16,5 @@ self.onmessage = function (e) {
 	xhr.onerror = function() {
 		self.postMessage({ iconUrl, loaded: false });
 	};
-	xhr.open('GET', iconUrl, true);
 	xhr.send();
 };
