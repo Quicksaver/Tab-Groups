939e3d9731d590a58279c87d2f66bcad22617d90
Luís Miguel <quicksaver@gmail.com>
2016-11-27 11:48:10 +0000

    Closes #475 change default previous group shortcut to use ~

--- resource/defaults.js
+++ resource/defaults.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// VERSION 1.4.23
+// VERSION 1.4.24
 
 objName = 'tabGroups';
 objPathString = 'tabgroups';
@@ -54,7 +54,7 @@ prefList = {
 	nextGroupAlt: false,
 	nextGroupCtrl: DARWIN,
 
-	previousGroupKeycode: '`',
+	previousGroupKeycode: '~',
 	previousGroupAccel: !DARWIN,
 	previousGroupShift: true,
 	previousGroupAlt: false,
